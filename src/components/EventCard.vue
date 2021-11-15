4f30859551d7b17d37a6852aece3c33422997aef
Neeleshkumarsaini <46334962+Neeleshkumarsaini@users.noreply.github.com>
2021-11-15 17:28:18 +0530

    Update EventCard.vue

--- src/components/EventCard.vue
+++ src/components/EventCard.vue
@@ -1,11 +1,12 @@
 <template>
-  <router-link to="/event/123">
+  <router-link class="event-link" :to="{ name: 'EventDetails', params: { id: event.id } }">
   <div class="event-card">
     <!-- Display event data  -->
- 
+    <img v-bind:src="image" alt="movie-poster">
     <h4>{{ event.original_title }}</h4>
     <p><strong>overview: </strong> {{ event.overview }}</p>
     <p><strong>votes: </strong> {{event.vote_count}}</p>
+    <!-- <span>{{ event.genres[0].name }}</span> -->
   
   </div>
 </router-link>
@@ -19,7 +20,16 @@ export default {
        required: true
      }
    },
-
+   data(){
+     return {
+       poster: 'https://image.tmdb.org/t/p/w154'
+     }
+   },
+   computed: {
+    image(){
+      return this.poster + this.event.poster_path
+    }
+  }
 }
 </script>
 
@@ -35,5 +45,9 @@ export default {
 .event-card:hover {
   transform: scale(1.01);
   box-shadow: 0 3px 12px 0 rgba(0, 0, 0, 0.2);
+}
+.event-link {
+  color: #2c3e50;
+  text-decoration: none;
 }
 </style>
