a118945fd45960c9a9a3235534a8260deae5bd47
Neeleshkumarsaini <46334962+Neeleshkumarsaini@users.noreply.github.com>
2021-11-16 18:55:00 +0530

    Update EventDetails.vue

--- src/views/EventDetails.vue
+++ src/views/EventDetails.vue
@@ -1,5 +1,5 @@
 <template>
-  <div>
+  <div v-if="event">
     <h1>{{ event.original_title }}</h1>
     <img v-bind:src="image" alt="movie-poster">
     <p>{{ event.overview }}</p>
@@ -10,29 +10,19 @@
 
     <h2>Cinema Halls</h2>
     <ul class="Halls-list">
-      <li>
-        <h3>Noida</h3>
-        <p v-if="tickets[0] > 0">Tickets Available: {{tickets[0]}}</p>
+      <li v-for="(availaible,index) in availability" :key="index">
+        <h3>{{ availaible.city }}</h3>
+        <p v-if="availaible.tickets > 0"> Tickets Available: {{ availaible.tickets }}</p>
         <p v-else>All Tickets Booked!!!</p>
-        <button class="button" v-bind:class="{ disabledButton: tickets[0]==0 }" v-on:click="bookTicketNoida">Book Ticket
-        </button>
-      </li>
-      <li>
-        <h3>Delhi</h3>
-        <p v-if="tickets[1] > 0">Tickets Available: {{tickets[1]}}</p>
-        <p v-else>All Tickets Booked!!!</p>
-        <button class="button" v-bind:class="{ disabledButton: tickets[1]==0 }" v-on:click="bookTicketDelhi">Book Ticket
-        </button>
-      </li>
-      <li>
-        <h3>Jaipur</h3>
-        <p v-if="tickets[2] > 0">Tickets Available: {{tickets[2]}}</p>
-        <p v-else>All Tickets Booked!!!</p>
-        <button class="button" v-bind:class="{ disabledButton: tickets[2]==0 }" v-on:click="bookTicketJaipur">Book Ticket
-        </button>
       </li>
     </ul>
-    
+    <span> <button class="button" v-bind:class="{ disabledButton: availability[0].tickets==0 }" v-on:click="bookTicketNoida">Book Noida Ticket 
+        </button>
+        <button class="button" v-bind:class="{ disabledButton: availability[1].tickets==0 }" v-on:click="bookTicketDelhi">Book Delhi Ticket 
+        </button>
+        <button class="button" v-bind:class="{ disabledButton: availability[2].tickets==0 }" v-on:click="bookTicketJaipur">Book Jaipur Ticket 
+        </button>
+    </span>
   </div>
 </template>
 
@@ -43,24 +33,36 @@ export default {
   data() {
     return {
       event: null,
-      tickets: [1,2,3],
+      availability: [
+        {city: 'Noida',
+          tickets: 1
+        },
+        {
+          city: 'Delhi',
+          tickets: 2
+        },
+        {
+          city: 'Jaipur',
+          tickets: 3
+        }
+      ],
       poster: 'https://image.tmdb.org/t/p/w342'
     }
   },
   methods: {
     bookTicketNoida(){
-      if(this.tickets[0] > 0){
-        this.tickets[0] -=1
+      if(this.availability[0].tickets > 0){
+        this.availability[0].tickets -=1
       }
     },
     bookTicketDelhi(){
-      if(this.tickets[1] > 0){
-        this.tickets[1] -=1
+      if(this.availability[1].tickets > 0){
+        this.availability[1].tickets -=1
       }
     },
     bookTicketJaipur(){
-      if(this.tickets[2] > 0){
-        this.tickets[2] -=1
+      if(this.availability[2].tickets > 0){
+        this.availability[2].tickets -=1
       }
     }
   },
@@ -69,12 +71,12 @@ export default {
       return this.poster + this.event.poster_path
     }
   },
-  created() {
+  mounted() {
     // fetch event and set local event data
     EventService.getEvent(this.id)
 	  .then(response => {
 		  this.event = response.data
-      //console.log(response.data)
+      console.log(response.data)
 	  })
 	  .catch(error => {
 		  console.log(error)
