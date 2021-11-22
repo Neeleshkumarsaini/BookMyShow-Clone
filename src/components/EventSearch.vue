4409ad5dd130d148d275bfea9f763413c3d6f155
Neeleshkumarsaini <46334962+Neeleshkumarsaini@users.noreply.github.com>
2021-11-22 10:40:43 +0530

    Added search functionality

---
+++ src/components/EventSearch.vue
@@ -0,0 +1,45 @@
+<template>
+  <div class="search">
+    <input type='text' placeholder="search movies" v-model='query' @keyup='getResult(query)'>
+    <div v-for='result in results' :key='result.id'>
+    <router-link :to="{ name: 'EventDetails', params: { id: result.id } }">
+   <p>{{result.title}}</p>
+  </router-link>
+  </div>
+  
+  </div>
+  
+</template>
+<script>
+  import axios from 'axios';
+  export default {
+    name: 'search',
+    data () {
+      return {
+      query: '',
+      results: '',
+      // poster: 'http://image.tmdb.org/t/p/w200/'
+    }
+ },
+  methods: {
+    getResult(query) {
+      axios.get('https://api.themoviedb.org/3/search/movie?api_key=af887aca762fa26e9947081957eeadef&query=' + query)
+      .then(response => {
+        this.results = response.data.results 
+        }
+      )
+    console.log(query)
+    }
+  },
+  }
+
+</script>
+
+<style>
+  .search{
+    /* border: 1px solid; */
+    margin: auto;
+    width: 10%;
+    /* margin-bottom: 10px; */
+  }
+</style>
