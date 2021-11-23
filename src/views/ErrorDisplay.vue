a3f14b28183b5b8172b2efdf53e5e9a8f2e0dd30
Neeleshkumarsaini <46334962+Neeleshkumarsaini@users.noreply.github.com>
2021-11-23 18:47:08 +0530

    to show the error if it occurs

---
+++ src/views/ErrorDisplay.vue
@@ -0,0 +1,18 @@
+<template>
+<div class="error-message">
+    <h4>Oops! There was an error</h4>
+    <p>{{ error }}</p>
+</div>
+</template>
+
+<script>
+    export default {
+        props: ['error']
+    }
+</script>
+
+<style>
+    .error-message{
+        text-align: center;
+    }
+</style>
