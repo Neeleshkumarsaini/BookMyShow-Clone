d5f0288fc5bf006e254b1fceb76e05a164f086a3
Neeleshkumarsaini <46334962+Neeleshkumarsaini@users.noreply.github.com>
2021-11-23 18:50:18 +0530

    added actions and mutations to fetch movies, movies by id and name

--- src/store/index.js
+++ src/store/index.js
@@ -3,24 +3,64 @@ import EventService from '@/services/EventService.js'
 
 export default createStore({
   state: {
-    user: 'neelesh',
+    user: 'Neelesh',
     events: [],
+    event: {},
+    search: []
   },
   mutations: {
-    SET_EVENTS(state, events){
-      state.events = events.results
+    SET_EVENTS(state, payload){
+      state.events = payload.results
+    },
+    SET_EVENT(state, payload){
+      state.event = payload
+    },
+    SET_SEARCH_EVENTS(state, payload){
+      state.search = payload
     }
   },
   actions: {
     fetchEvents({ commit }, page){
-      EventService.getEvents(page)
+      return EventService.getEvents(page)
 	  .then(response => {
       commit('SET_EVENTS', response.data)
+		  
 	  })
 	  .catch(error => {
-		  console.log(error)
+		  throw(error)
 	  })
     },
+    fetchEvent({ commit, state }, id){
+      const existingEvent = state.events.find(event => event.id === id)
+      if(existingEvent){
+        commit('SET_EVENT', existingEvent)
+      }
+      else{
+        return EventService.getEvent(id)
+	        .then(response => {
+          commit('SET_EVENT', response.data)
+		      
+	      })
+	      .catch(error => {
+		    throw(error)
+	    })
+      } 
+    },
+    fetchSearchEvents({ commit, state }, val){
+      const existingEvent = state.events.find(event => event.title === val)
+      if(existingEvent){
+        commit('SET_EVENT', existingEvent)
+      }
+      else{
+        EventService.getSearch(val)
+        .then(response => {
+          commit('SET_SEARCH_EVENTS', response.data.results)
+        })
+        .catch(error => {
+          console.log(error)
+        })
+      }
+    }
   },
   modules: {},
 });
