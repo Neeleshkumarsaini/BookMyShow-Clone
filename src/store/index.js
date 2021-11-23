49fa268c3b5dc9eb916bd01aa18b1bd067f7080a
Neeleshkumarsaini <46334962+Neeleshkumarsaini@users.noreply.github.com>
2021-11-23 10:59:58 +0530

    Update index.js

--- src/store/index.js
+++ src/store/index.js
@@ -1,10 +1,26 @@
 import { createStore } from "vuex";
+import EventService from '@/services/EventService.js'
 
 export default createStore({
   state: {
-    user: 'neelesh'
+    user: 'neelesh',
+    events: [],
   },
-  mutations: {},
-  actions: {},
+  mutations: {
+    SET_EVENTS(state, events){
+      state.events = events.results
+    }
+  },
+  actions: {
+    fetchEvents({ commit }, page){
+      EventService.getEvents(page)
+	  .then(response => {
+      commit('SET_EVENTS', response.data)
+	  })
+	  .catch(error => {
+		  console.log(error)
+	  })
+    },
+  },
   modules: {},
 });
