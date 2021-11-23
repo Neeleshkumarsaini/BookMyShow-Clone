2663eefb980b540b74cd8cd2732ba357464644c7
Neeleshkumarsaini <46334962+Neeleshkumarsaini@users.noreply.github.com>
2021-11-23 18:55:45 +0530

    added bootstrap

--- src/components/EventSearch.vue
+++ src/components/EventSearch.vue
@@ -1,10 +1,12 @@
 <template>
   <div class="search">
-    <input type='text' placeholder="search movies" v-model='query'>
+    <!-- <input type='text' placeholder="search movies" v-model='query'> -->
+    <input class="form-control mr-sm-2" type="search" placeholder="search movie" aria-label="Search" v-model='query'>
     <div v-for='result in results' :key='result.id'>
     <router-link :to="{ name: 'EventDetails', params: { id: result.id } }">
    <p>{{result.title}}</p>
-   <img v-bind:src="'http://image.tmdb.org/t/p/w185/' +  result.poster_path">
+
+   <!-- <img v-bind:src="'http://image.tmdb.org/t/p/w185/' +  result.poster_path"> -->
   </router-link>
   </div>
   
@@ -12,26 +14,48 @@
   
 </template>
 <script>
-  import axios from 'axios';
+  // import axios from 'axios';
+  // import EventService from "@/services/EventService.js";
   export default {
     name: 'search',
     data () {
       return {
       query: '',
-      results: '',
+      // results: '',
       // poster: 'http://image.tmdb.org/t/p/w500/'
     }
  },
   watch: {
     query(val) {
-      axios.get('https://api.themoviedb.org/3/search/movie?api_key=af887aca762fa26e9947081957eeadef&query=' + val)
-      .then(response => {
-        this.results = response.data.results
-        }
-      )
-    console.log(val)
+      this.$store.dispatch('fetchSearchEvents', val)
+        .catch(error => {
+          this.$router.push({
+            name: 'ErrorDisplay',
+            params: { error: error}
+          })
+        })
+    //   EventService.getSearch(val)
+	  //  .then(response => {
+		//    this.results = response.data.results
+    //    //console.log(response.data.results)
+	  //  })
+	  //  .catch(error => {
+		//    console.log(error)
+	  //  })
+
+    //   axios.get('https://api.themoviedb.org/3/search/movie?api_key=af887aca762fa26e9947081957eeadef&query=' + val)
+    //   .then(response => {
+    //     this.results = response.data.results
+    //     }
+    //   )
+    // console.log(val)
     }
   },
+  computed:{
+    results(){
+      return this.$store.state.search
+    }
+  }
   }
 
 </script>
@@ -40,7 +64,7 @@
   .search{
     /* border: 1px solid; */
     margin: auto;
-    width: 10%;
+    width: 30%;
     /* margin-bottom: 10px; */
     
   }
