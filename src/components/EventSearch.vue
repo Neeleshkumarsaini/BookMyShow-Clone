cca4aee8d317216e9efb4b0d448f1dd8b9bea4f5
Neeleshkumarsaini <46334962+Neeleshkumarsaini@users.noreply.github.com>
2021-11-24 19:08:25 +0530

    Update EventSearch.vue

--- src/components/EventSearch.vue
+++ src/components/EventSearch.vue
@@ -1,11 +1,10 @@
 <template>
   <div class="search">
-    <!-- <input type='text' placeholder="search movies" v-model='query'> -->
     <input class="form-control mr-sm-2" type="search" placeholder="search movie" aria-label="Search" v-model='query'>
     <div v-for='result in results' :key='result.id'>
     <router-link :to="{ name: 'EventDetails', params: { id: result.id } }">
    <p>{{result.title}}</p>
-
+  
    <!-- <img v-bind:src="'http://image.tmdb.org/t/p/w185/' +  result.poster_path"> -->
   </router-link>
   </div>
@@ -16,45 +15,33 @@
 <script>
   // import axios from 'axios';
   // import EventService from "@/services/EventService.js";
+  import { mapState } from 'vuex';
   export default {
     name: 'search',
     data () {
       return {
       query: '',
-      // results: '',
-      // poster: 'http://image.tmdb.org/t/p/w500/'
+      
     }
  },
   watch: {
     query(val) {
       this.$store.dispatch('fetchSearchEvents', val)
         .catch(error => {
+          this.$store.dispatch('fetchError', error)
           this.$router.push({
             name: 'ErrorDisplay',
-            params: { error: error}
+            
           })
         })
-    //   EventService.getSearch(val)
-	  //  .then(response => {
-		//    this.results = response.data.results
-    //    //console.log(response.data.results)
-	  //  })
-	  //  .catch(error => {
-		//    console.log(error)
-	  //  })
-
-    //   axios.get('https://api.themoviedb.org/3/search/movie?api_key=af887aca762fa26e9947081957eeadef&query=' + val)
-    //   .then(response => {
-    //     this.results = response.data.results
-    //     }
-    //   )
-    // console.log(val)
+   
     }
   },
   computed:{
-    results(){
-      return this.$store.state.search
-    }
+    ...mapState({
+      results: 'search'
+    })
+    
   }
   }
 
