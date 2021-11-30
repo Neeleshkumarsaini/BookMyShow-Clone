84c10069acebdd1d2d71ecb04bc3847912312846
Neeleshkumarsaini <46334962+Neeleshkumarsaini@users.noreply.github.com>
2021-11-30 11:07:58 +0530

    Update EventSearch.vue

--- src/components/EventSearch.vue
+++ src/components/EventSearch.vue
@@ -1,15 +1,6 @@
 <template>
   <div class="search">
-   
     <input class="form-control mr-sm-2" type="search" placeholder="search movie" aria-label="Search" v-model='query'>
-    <div v-for='result in results' :key='result.id'>
-    <router-link :to="{ name: 'EventDetails', params: { id: result.id } }">
-   <p>{{result.title}}</p>
- 
-   <!-- <img v-bind:src="'http://image.tmdb.org/t/p/w185/' +  result.poster_path"> -->
-  </router-link>
-  </div>
-  
   </div>
   
 </template>
@@ -22,7 +13,6 @@
     data () {
       return {
       query: '',
-      
     }
  },
   watch: {
@@ -32,17 +22,17 @@
           this.$store.commit('SET_ERROR', error)
           this.$router.push({
             name: 'ErrorDisplay',
-           
+            
           })
         })
-   
+    
     }
   },
   computed:{
     ...mapState({
       results: 'search'
     })
-   
+    
   }
   }
 
