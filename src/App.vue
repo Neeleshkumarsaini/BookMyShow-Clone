c49329b2bb35a79b84926fcf9911cc6709870b0d
Neeleshkumarsaini <46334962+Neeleshkumarsaini@users.noreply.github.com>
2021-12-03 19:45:42 +0530

    Update App.vue

--- src/App.vue
+++ src/App.vue
@@ -1,24 +1,21 @@
 <template>
   <div id="app">
-  <!-- <div class="nav-bar"><h1>BookMyShow</h1> 
-    
-  </div> -->
-  <!-- Image and text -->
-<nav class="navbar navbar-light bg-light">
-  <a class="navbar-brand" href="#">
-    <img src="https://www.apkmirror.com/wp-content/uploads/2021/02/40/60322adf14a17.png" width="30" height="30" class="d-inline-block align-top" alt="">
-    BookMyShow
-  </a>
-</nav>
+  <app-nav />
 
   <div id="nav">
-    <router-link :to="{ name: 'EventList' }">Events</router-link> | 
     <router-link :to="{ name: 'About' }">About</router-link>
   </div>
   <router-view />
   </div>
 </template>
 
+<script>
+import AppNav from './components/AppNav'
+
+export default {
+  components: { AppNav }
+}
+</script>
 
 <style>
 #app {
@@ -26,7 +23,9 @@
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
   
+  /* padding: 10px; */
   color: #2c3e50;
+  /* background-color: #92a8d1; */
 }
 
 #nav {
@@ -45,9 +44,11 @@
 .navbar {
   background: linear-gradient(-90deg, #16c0b0,#cf6a88 );
   height: 80px;
+ 
   -webkit-box-shadow: 0px 2px 15px -12px rgba(0, 0, 0, 0.57);
   -moz-box-shadow: 0px 2px 15px -12px rgba(0, 0, 0, 0.57);
   box-shadow: 1px 1px 5px rgba(0, 0, 0, 0.57);
+
 }
 
 </style>
