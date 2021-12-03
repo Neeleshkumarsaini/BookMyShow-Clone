59a89a553608a07e532889cbf405407eddd836c6
Neeleshkumarsaini <46334962+Neeleshkumarsaini@users.noreply.github.com>
2021-12-03 10:24:30 +0530

    Create RegisterUser.vue

---
+++ src/views/RegisterUser.vue
@@ -0,0 +1,33 @@
+<template>
+
+      <form>
+      <div class="form-group">
+      <label for="exampleInputEmail1">Email address</label>
+      <input v-model="email" type="email" class="form-control" id="exampleInputEmail1" aria-describedby="emailHelp">
+      <small id="emailHelp" class="form-text text-muted">We'll never share your email with anyone else.</small>
+      </div>
+    <div class="form-group">
+    <label for="exampleInputPassword1">Password</label>
+    <input v-model="password" type="password" class="form-control" id="exampleInputPassword1">
+    </div>
+ 
+    <button type="submit" class="btn btn-primary">Register</button>
+    </form>
+    </template>
+    
+    <script>
+        export default{
+            data(){
+                return {   
+                  email: '',
+                  password: ''
+                }
+            },
+            methods: {
+            register () {
+            this.$store
+            .dispatch('userRegisteration', {  email: this.email, password: this.password })
+          }
+        }
+      }    
+    </script>
