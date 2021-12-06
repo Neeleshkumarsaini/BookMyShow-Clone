d8f60a81256f9cbb7191c0779064aedcf7e40941
Neeleshkumarsaini <46334962+Neeleshkumarsaini@users.noreply.github.com>
2021-12-06 19:11:42 +0530

    Create Home.vue

---
+++ src/views/Home.vue
@@ -0,0 +1,27 @@
+<template>
+  <div class="home">
+    <h1>Welcome to Book My Show</h1>
+    <div v-if="!loggedIn">
+      Please
+      <router-link to="/login">Login</router-link>
+      or
+      <router-link to="/register">Register</router-link>
+      to continue
+    </div>
+  </div>
+</template>
+
+<script>
+import { mapGetters } from 'vuex'
+export default {
+  computed: {
+  ...mapGetters(['loggedIn'])
+  }
+}
+</script>
+
+<style scoped>
+    .home{
+        text-align: center;
+    }
+</style>
