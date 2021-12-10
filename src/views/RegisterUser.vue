dcc91644327b589a871db48b97056526096d1221
Neeleshkumarsaini <46334962+Neeleshkumarsaini@users.noreply.github.com>
2021-12-10 19:36:03 +0530

    Update RegisterUser.vue

--- src/views/RegisterUser.vue
+++ src/views/RegisterUser.vue
@@ -1,28 +1,30 @@
 <template>
-    
-      <form @submit.prevent="register">
+  <div class="registerForm">
+    <h1>Register</h1>
+    <form @submit.prevent="register">
       <div class="form-group">
-      <label for="exampleInputEmail1">Email address</label>
-      <input v-model="email" type="email" class="form-control" id="exampleInputEmail1" aria-describedby="emailHelp">
-      <small id="emailHelp" class="form-text text-muted">We'll never share your email with anyone else.</small>
+        <label for="exampleInputEmail1">Email address</label>
+        <input v-model="email" type="email" class="form-control" id="exampleInputEmail1" aria-describedby="emailHelp">
+        <small id="emailHelp" class="form-text text-muted">We'll never share your email with anyone else.</small>
       </div>
-    <div class="form-group">
-    <label for="exampleInputPassword1">Password</label>
-    <input v-model="password" type="password" class="form-control" id="exampleInputPassword1">
-    </div>
+      <div class="form-group">
+        <label for="exampleInputPassword1">Password</label>
+        <input v-model="password" type="password" class="form-control" id="exampleInputPassword1">
+      </div>
  
-    <button type="submit" class="btn btn-primary">Register</button>
-    <ul class="errors">
+      <button type="submit" class="btn btn-primary">Register</button>
+      <ul v-if="errors!==null" class="errors">
         <li v-for="(error, index) in errors" :key="index">
           {{ error }}
         </li>
-    </ul>
-    <div>
-    <router-link to="/login">
-        Already have an account? Login.
-      </router-link>
-    </div>  
+      </ul>
+      <div>
+        <router-link to="/login">
+          Already have an account? Login.
+        </router-link>
+      </div>  
     </form>
+  </div>
 </template>
     
 <script>
@@ -51,5 +53,9 @@
 <style scoped>
   .errors{
     color: red;
+  }
+  .registerForm{
+    margin: auto;
+    width: 30%;
   }
 </style>
