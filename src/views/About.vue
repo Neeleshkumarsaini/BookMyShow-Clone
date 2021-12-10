0248885191975e7f6bfd25bcfb2764a9254e33bb
Neeleshkumarsaini <46334962+Neeleshkumarsaini@users.noreply.github.com>
2021-12-10 19:42:14 +0530

    Update About.vue

--- src/views/About.vue
+++ src/views/About.vue
@@ -1,5 +1,14 @@
 <template>
   <div class="about">
     <h1>This is an about page</h1>
+    <p>
+      This project is made only for learning purpose!!!
+    </p>
   </div>
 </template>
+
+<style scoped>
+  .about{
+    text-align: center;
+  }
+</style>
